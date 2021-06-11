54dff013a04905b8a6339d044b4cc79725116e48
Patrick Lee <patricklee1138@gmail.com>
2021-06-11 09:20:39 -0700

    adjust initial project settings

--- ParkAPI/Startup.cs
+++ ParkAPI/Startup.cs
@@ -44,7 +44,7 @@ namespace ParkAPI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ParkAPI v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
