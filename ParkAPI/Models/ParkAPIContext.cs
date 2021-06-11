7ff56a7c1c49a9fc01711240cb032a33cfec9392
Patrick Lee <patricklee1138@gmail.com>
2021-06-11 09:51:40 -0700

    add context file

---
+++ ParkAPI/Models/ParkAPIContext.cs
@@ -0,0 +1,12 @@
+namespace ParkAPI.Models
+{
+    public class ParkAPIContext : DbContext
+    {
+        public ParkAPIContext(DbContextOptions<ParkAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Park> Parks { get; set; }
+    }
+}
