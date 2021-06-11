77021d85cead089fa3bb44587569e16213c74a78
Patrick Lee <patricklee1138@live.com>
2021-06-11 11:59:35 -0700

    add 2nd entry to database

--- ParkAPI/Models/ParkAPIContext.cs
+++ ParkAPI/Models/ParkAPIContext.cs
@@ -21,6 +21,15 @@ namespace ParkAPI.Models
                         Description = "(text courtesy of stateparks.oregon.gov) Diggers, this park's for you! Razor clamming is a favorite activity as well as surfing. If you plan to visit prime Newport attractions like the Oregon Coast Aquarium and Hatfield Marine Science Center, you must stop in for a refreshing picnic at Agate Beach.",
                         Url = "https://stateparks.oregon.gov/index.cfm?do=park.profile&parkId=152",
                         Phone = "541-265-4560"
+                    },
+                    new Park {
+                        ParkId = 2,
+                        Name = "Alderwood",
+                        Type = "State",
+                        Location = "Near Eugene, Oregon, United States",
+                        Description = "(text courtesy of stateparks.oregon.gov) Large trees characterize this forested park along Hwy 36 between Junction City and Triangle Lake. There's a picnic area, restroom, and short trail along the Long Tom River. Bring a lunch and relax!",
+                        Url = "https://stateparks.oregon.gov/index.cfm?do=park.profile&parkId=59",
+                        Phone = "541-937-1173"
                     }
                 );
         }
